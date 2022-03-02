f3f2b69310f14bf55938402660cac3111f3893f3
agilakbarm <90818053+agilakbarm@users.noreply.github.com>
2022-03-02 07:30:25 +0700

    Add files via upload

---
+++ variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+// definisikan variables
+$nama ='Muhamad Agil Akbar Maulana';
+$umur = 20;
+$berat = 65;
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+echo "<br/>Hello $nama Apa kabar?";
+?>
